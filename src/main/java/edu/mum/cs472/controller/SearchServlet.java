e3198c8a706edf8dac9b826de8fe69265946eda7
BEREKET ABRAHAM <bereketmichael741@gmail.com>
2019-08-14 15:11:48 -0500

    User Filter

--- src/main/java/edu/mum/cs472/controller/SearchServlet.java
+++ src/main/java/edu/mum/cs472/controller/SearchServlet.java
@@ -6,6 +6,7 @@ import edu.mum.cs472.model.Room;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@WebServlet("/search")
 public class SearchServlet extends HttpServlet {
 
 	private DAO data;
