1a3f4f9a778df80817fd72028210318472cae924
aserahmad <53275422+aserahmad@users.noreply.github.com>
2019-08-14 22:12:38 -0500

    Add files via upload

--- src/main/java/edu/mum/cs472/controller/LogOutServlet.java
+++ src/main/java/edu/mum/cs472/controller/LogOutServlet.java
@@ -1,16 +1,24 @@
 package edu.mum.cs472.controller;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import java.io.IOException;
 
+@WebServlet("/logout")
 public class LogOutServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // end the session and go back to the index page
-        req.getSession().invalidate();
-        resp.sendRedirect("index.jsp");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // end the session, erase current user from session attributes and cookies, and go back to the index page
+        request.getSession().invalidate();
+        HttpSession session = request.getSession();
+        session.setAttribute("name", "");
+        for (Cookie c : request.getCookies()) {
+                if (c.getName().equals("userCookie")) {
+                    c.setMaxAge(0);
+                break;
+            }
+        }
+        response.sendRedirect("index.jsp");
     }
 }
