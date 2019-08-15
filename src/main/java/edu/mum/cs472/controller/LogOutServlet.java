07ef672aeddab6a4c40ddb89d82a30035ca622e3
aserahmad <53275422+aserahmad@users.noreply.github.com>
2019-08-15 10:53:17 -0500

    Add files via upload

--- src/main/java/edu/mum/cs472/controller/LogOutServlet.java
+++ src/main/java/edu/mum/cs472/controller/LogOutServlet.java
@@ -10,15 +10,16 @@ public class LogOutServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // end the session, erase current user from session attributes and cookies, and go back to the index page
-        request.getSession().invalidate();
         HttpSession session = request.getSession();
         session.setAttribute("name", "");
-        for (Cookie c : request.getCookies()) {
-                if (c.getName().equals("userCookie")) {
-                    c.setMaxAge(0);
-                break;
-            }
-        }
+        session.invalidate();
+
+//        for (Cookie c : request.getCookies()) {
+//                if (c.getName().equals("userCookie")) {
+//                    c.setMaxAge(0);
+//                break;
+//            }
+//        }
         response.sendRedirect("index.jsp");
     }
 }
